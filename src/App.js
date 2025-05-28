9643a6627b4b2137ddb5ef87f571382571004d3d
Elvin Nguyen <elvinnguyen@mac.local.ftr>
2025-05-28 16:11:14 -0700

    changed ChampionInfo to ChampionInfo.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChampionList from "./components/ChampionList";
-import ChampionInfo from "./components/ChampionInfo";
+import ChampionInfo from "./components/ChampionInfo.js";
 
 function App() {
   return (
